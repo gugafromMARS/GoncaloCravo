f584f84184f1f641334e1fc754f1bedf04d57571
Gonçalo <goncaloscravo@gmail.com>
2024-03-30 18:49:08 +0000

    Add: base structure

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import About from "./pages/about/About.jsx";
+import Home from "./pages/home/Home.jsx";
+import Portfolio from "./pages/portfolio/Portfolio.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
